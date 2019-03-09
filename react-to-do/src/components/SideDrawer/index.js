3b5e55599631b1e7c532f49fb27a3f2f289050ba
ksterle <krunoslavsterle@gmail.com>
2019-03-09 21:29:19 +0100

    - SideDrawer; DrawerToggle

--- react-to-do/src/components/SideDrawer/index.js
+++ react-to-do/src/components/SideDrawer/index.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 
-const SideDrawer = props => <div>Side Drawer</div>;
+import classes from "./index.module.css";
+import DrawerToggle from "./DrawerToggle";
+
+class SideDrawer extends Component {
+  state = {
+    opened: false
+  };
+
+  drawerToggleHandler = () => {
+    console.log("toggle clicked");
+    this.setState(prevState => {
+      return { opened: !prevState.opened };
+    });
+  };
+
+  render() {
+    let drawerClasses = [classes.SideDrawer, classes.Closed];
+    if (this.state.opened) {
+      drawerClasses = [classes.SideDrawer, classes.Opened];
+    }
+
+    return (
+      <div className={drawerClasses.join(" ")}>
+        <DrawerToggle clicked={this.drawerToggleHandler} />
+        <DrawerToggle />
+      </div>
+    );
+  }
+}
 
 export default SideDrawer;
