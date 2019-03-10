18cddfae2ced4beaea84a8b2e894981ec9f45151
ksterle <krunoslavsterle@gmail.com>
2019-03-10 12:43:30 +0100

    - adding State to Tasks component

--- react-to-do/src/components/Tasks/Task/index.js
+++ react-to-do/src/components/Tasks/Task/index.js
@@ -5,17 +5,17 @@ import classes from "./index.module.css";
 const Task = props => {
   const iconClasses = ["material-icons", "icon", "icon-md", classes.CheckIcon];
   let checkIcon = "check_box_outline_blank";
-  if (true) {
+  if (props.completed) {
     checkIcon = "check_box";
   }
 
   return (
     <div className={classes.Task}>
       <div className={classes.TaskBody}>
-        <i className={iconClasses.join(" ")} onClick={props.clicked}>
+        <i className={iconClasses.join(" ")} onClick={props.stateChanged}>
           {checkIcon}
         </i>
-        <div className={classes.TaskButton}>This is my Task!</div>
+        <div className={classes.TaskButton}>{props.description}</div>
       </div>
     </div>
   );
--- react-to-do/src/components/Tasks/index.js
+++ react-to-do/src/components/Tasks/index.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./index.module.css";
 import Task from "./Task";
 
-const Tasks = props => (
-  <div className={classes.Tasks}>
-    <Task />
-    <Task />
-    <Task />
-    <Task />
-  </div>
-);
+class Tasks extends Component {
+  state = {
+    tasks: [
+      { id: 1, description: "This is my first task", completed: false },
+      { id: 2, description: "This is my second task", completed: true },
+      { id: 3, description: "Buy some milk", completed: true },
+      { id: 4, description: "Study React", completed: false }
+    ]
+  };
+
+  taskStateChangedHandler = id => {
+    this.setState(prevState => {
+      var updatedTasks = [...this.state.tasks];
+
+      var task = updatedTasks.find(t => t.id === id);
+      task.completed = !task.completed;
+
+      return { tasks: updatedTasks };
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.Tasks}>
+        {this.state.tasks.map(task => {
+          return (
+            <Task
+              key={task.id}
+              description={task.description}
+              completed={task.completed}
+              stateChanged={() => this.taskStateChangedHandler(task.id)}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default Tasks;
